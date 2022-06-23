b9a1a1781c18d21be4fee4984d0fb9b1b1c19581
Joseph Robertson <josephsmac@MacBook-Air.local>
2022-06-23 16:07:52 -0400

    Added the ability to look up and added a roof to the main room

--- Assets/Scripts/FlashLightScript.cs
+++ Assets/Scripts/FlashLightScript.cs
@@ -13,31 +13,44 @@ public class FlashLightScript : MonoBehaviour {
     public float rotationSpeed; // The rotation speed of the flashlight
     public float minimum_Y_value; // The farthest to the left the flashlight can go
     public float maximum_Y_value; // The farthest to the right the flashlight can go
+    public float minimum_X_value;
+    public float maximum_X_value;
     public float rotationDirection; // The direction the flashlight is currently rotating
-    float currentRotation; // The current direction the flashlight is facing
-    float playerCurrentRotation; // The current direction the player is facing
+    public float currentYRotation; // The current direction the flashlight is facing
+    public float currentXRotation;
+    public float playerCurrentYRotation; // The current direction the player is facing
+    public float playerCurrentXRotation;
 
     void FixedUpdate() {
         // Changes the players rotation values from 0 - 360 to -180 - 180
         if(player.eulerAngles.y <= 180f)
         {
-            playerCurrentRotation = player.eulerAngles.y;
+            playerCurrentYRotation = player.eulerAngles.y;
         }
         else
         {
-            playerCurrentRotation = player.eulerAngles.y - 360f;
+            playerCurrentYRotation = player.eulerAngles.y - 360f;
+        }
+
+        if(player.eulerAngles.x <= 180f)
+        {
+            playerCurrentXRotation = player.eulerAngles.x;
+        }
+        else
+        {
+            playerCurrentXRotation = player.eulerAngles.x - 360f;
         }
 
         // Changes the flashlights direction depending on what direction the player is facing
-        if(playerCurrentRotation < -42f) {
+        if(playerCurrentYRotation < -42f) {
             rotationDirection = -1f;
         }
-        else if(playerCurrentRotation > -25f) {
+        else if(playerCurrentYRotation > -25f) {
             rotationDirection = 1f;
         }
 
         // Turns the flashlight off if the player is not looking at one of the doors
-        if(playerCurrentRotation > 15) {
+        if(playerCurrentYRotation > 15) {
             GetComponent<Light>().enabled = false;
         }
         else {
@@ -49,30 +62,54 @@ public class FlashLightScript : MonoBehaviour {
         // Changes the objects rotation values from 0 - 360 to -180 - 180
         if(transform.eulerAngles.y <= 180f)
         {
-            currentRotation = transform.eulerAngles.y;
+            currentYRotation = transform.eulerAngles.y;
         }
         else
         {
-            currentRotation = transform.eulerAngles.y - 360f;
+            currentYRotation = transform.eulerAngles.y - 360f;
+        }
+
+        if(transform.eulerAngles.x <= 180f)
+        {
+            currentXRotation = transform.eulerAngles.x;
+        }
+        else
+        {
+            currentXRotation = transform.eulerAngles.x - 360f;
         }
 
         // Checks if the flashlight should be moving left and isn't at the minimum
-        if(rotationDirection < 0 && currentRotation > minimum_Y_value) {
+        if(rotationDirection < 0 && currentYRotation > minimum_Y_value) {
             // Rotates the flashlight
-            transform.rotation = Quaternion.Euler(2, transform.localRotation.eulerAngles.y + rotationSpeed*Time.deltaTime*rotationDirection, 0);
+            transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y + rotationSpeed*Time.deltaTime*rotationDirection, transform.localRotation.eulerAngles.z);
         }
         // Checks if the flashlight should be moving right and isn't at the maximum
-        else if(rotationDirection > 0 && currentRotation < maximum_Y_value) {
+        else if(rotationDirection > 0 && currentYRotation < maximum_Y_value) {
             // Rotates the flashlight
-            transform.rotation = Quaternion.Euler(2, transform.localRotation.eulerAngles.y + rotationSpeed*Time.deltaTime*rotationDirection, 0);
+            transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y + rotationSpeed*Time.deltaTime*rotationDirection, transform.localRotation.eulerAngles.z);
         }
         else {
             // Corrects for any overshooting caused by the speed of the flashlight by setting the flashlights rotation to it's minimum/maximum after the the flashlight stops 
             if(rotationDirection < 0) {
-                transform.rotation = Quaternion.Euler(2, minimum_Y_value, 0);
+                transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, minimum_Y_value, transform.localRotation.eulerAngles.z);
             }
             else {
-                transform.rotation = Quaternion.Euler(2, maximum_Y_value, 0);
+                transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, maximum_Y_value, transform.localRotation.eulerAngles.z);
+            }
+        }
+
+        if(playerCurrentXRotation < -10 && currentXRotation > minimum_X_value) {
+            transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x - rotationSpeed*Time.deltaTime,transform.localRotation.eulerAngles.y,transform.localRotation.eulerAngles.z);
+        }
+        else if(playerCurrentXRotation > -10 && currentXRotation < maximum_X_value) {
+            transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x + rotationSpeed*Time.deltaTime,transform.localRotation.eulerAngles.y,transform.localRotation.eulerAngles.z);
+        }
+        else {
+            if(playerCurrentXRotation < -10) {
+                transform.rotation = Quaternion.Euler(minimum_X_value,transform.localRotation.eulerAngles.y,transform.localRotation.eulerAngles.z);
+            }
+            else {
+                transform.rotation = Quaternion.Euler(maximum_X_value,transform.localRotation.eulerAngles.y,transform.localRotation.eulerAngles.z);
             }
         }
     }
--- Assets/Scripts/PlayerCameraMovement.cs
+++ Assets/Scripts/PlayerCameraMovement.cs
@@ -10,23 +10,50 @@ public class PlayerCameraMovement : MonoBehaviour
 {
     float maximum_Y_value = 35f; // The farthest the camera can move to the right
     float minimum_Y_value = -45f; // The farthest the camera can move to the left
+    float maximum_X_value = 8f;
+    float minimum_X_value = -75f;
     public float rotationSpeed = 10f; // How fast the camera rotates
-    float rotationDirection = 0f; // What direction is the camera currently rotating
-    public float currentRotation; // What is the current direction the camera is facing
+    public float rotationDirection = 0f; // What direction is the camera currently rotating
+    public float currentYRotation; // What is the current direction the camera is facing
+    public float currentXRotation;
     public float screenWidth; // How wide in pixles is the screen
+    public float screenHeight;
     public MonitorScript monitor; // The players monitor that pops up when the player moves all the way to the right
+    public bool isLookingUp = false;
 
     void FixedUpdate() {
         Vector3 mousePos = Input.mousePosition; // A 3 dimentional vector of the users mouse
         screenWidth = Screen.width; // setting the screen width
-        if(mousePos.x < screenWidth/20f) { // Check if the mouse is on the left side of the screen
+        screenHeight = Screen.height;
+        if(mousePos.y > screenHeight - screenHeight/20f && !isLookingUp) {
+            isLookingUp = true;
+        }
+        else if (mousePos.y < screenHeight/20f && isLookingUp) {
+            isLookingUp = false;
+        }
+        
+        if(mousePos.x < screenWidth/20f && !isLookingUp) { // Check if the mouse is on the left side of the screen
             rotationDirection = -1f; // Change the rotation direction to rotate left
         }
-        else if(mousePos.x > screenWidth - screenWidth/20f) { // Check if the mouse is on the right side of the screen
+        else if(mousePos.x > screenWidth - screenWidth/20f && !isLookingUp) { // Check if the mouse is on the right side of the screen
             rotationDirection = 1f; // Set the rotation direction to rotate right
         }
         else {
-            rotationDirection = 0f; // Set the rotation direction to 0 if the mouse is not on either side of the screen
+            if(!isLookingUp) {
+                rotationDirection = 0;
+            }
+            else {
+                if(currentYRotation > 0.3f) {
+                    rotationDirection = -1f;
+                }
+                else if(currentYRotation < -0.3f) {
+                    rotationDirection = 1f;
+                }
+                else {
+                    rotationDirection = 0;
+                    transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, 0, 0);
+                }
+            }
         }
     }
 
@@ -36,15 +63,24 @@ public class PlayerCameraMovement : MonoBehaviour
         // Changes the objects rotation values from 0 - 360 to -180 - 180
         if(transform.eulerAngles.y <= 180f)
         {
-            currentRotation = transform.eulerAngles.y;
+            currentYRotation = transform.eulerAngles.y;
         }
         else
         {
-            currentRotation = transform.eulerAngles.y - 360f;
+            currentYRotation = transform.eulerAngles.y - 360f;
+        }
+
+        if(transform.eulerAngles.x <= 180f)
+        {
+            currentXRotation = transform.eulerAngles.x;
+        }
+        else
+        {
+            currentXRotation = transform.eulerAngles.x - 360f;
         }
 
         // Makes the monitor appear and disappear depending on the players rotation
-        if(currentRotation > 30) {
+        if(currentYRotation > 30) {
             monitor.changeDirection(-1f);
         }
         else{
@@ -52,14 +88,27 @@ public class PlayerCameraMovement : MonoBehaviour
         }
 
         // Checks if the player should be moving left and isn't at the minimum
-        if(rotationDirection < 0 && currentRotation > minimum_Y_value) {
+        if(rotationDirection < 0 && currentYRotation > minimum_Y_value) {
             // Rotates the players camera
-            transform.rotation = Quaternion.Euler(15f, transform.localRotation.eulerAngles.y + rotationSpeed*Time.deltaTime*rotationDirection, 0);
+            transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y + rotationSpeed*Time.deltaTime*rotationDirection, 0);
         }
         // Checks if the player should be moving right and isn't at the maximum
-        else if(rotationDirection > 0 && currentRotation < maximum_Y_value) {
+        else if(rotationDirection > 0 && currentYRotation < maximum_Y_value) {
             // Rotates the players camera
-            transform.rotation = Quaternion.Euler(15f, transform.localRotation.eulerAngles.y + rotationSpeed*Time.deltaTime*rotationDirection, 0);
+            transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y + rotationSpeed*Time.deltaTime*rotationDirection, 0);
+        }
+
+        if(isLookingUp && currentXRotation > minimum_X_value) {
+            transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x - rotationSpeed*Time.deltaTime*1.5f, transform.localRotation.eulerAngles.y, 0);
+        }
+        else if(isLookingUp &&currentXRotation < minimum_X_value - 0.2f) {
+            transform.rotation = Quaternion.Euler(minimum_X_value, transform.localRotation.eulerAngles.y, 0);
+        }
+        else if(!isLookingUp && currentXRotation < maximum_X_value) {
+            transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x + rotationSpeed*Time.deltaTime*1.5f, transform.localRotation.eulerAngles.y, 0);
+        }
+        else if (!isLookingUp && currentXRotation > maximum_X_value + 0.2f) {
+            transform.rotation = Quaternion.Euler(maximum_X_value, transform.localRotation.eulerAngles.y, 0);
         }
     }
 }
